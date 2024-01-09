0751e8deff97bf98029a9f2f3376da4ede08d47c
Lorenzo Zuluaga <lorenzo.zuluaga@kommit.co>
2024-01-09 14:05:14 -0500

    Add basic gem structure

---
+++ lib/business_notification_system.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "business_notification_system/version"
+
+module BusinessNotificationSystem
+  class Error < StandardError; end
+  # Your code goes here...
+end
