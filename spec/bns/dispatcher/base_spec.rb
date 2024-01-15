a632a5cbe6effafb1a75ecae8b5d469ce4ccbdf4
Lorenzo Zuluaga <lorenzo.zuluaga@kommit.co>
2024-01-15 15:19:57 -0500

    Include basic test for base classes and modules

---
+++ spec/bns/dispatcher/base_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+RSpec.describe Dispatcher::Base do
+  before do
+    @config = { webhook: "https://example.com/webhook", name: "Example Name" }
+    @dispatcher = described_class.new(@config)
+  end
+
+  describe "Arguments and methods" do
+    it { expect(@dispatcher).to respond_to(:webhook) }
+    it { expect(@dispatcher).to respond_to(:name) }
+
+    it { expect(described_class).to respond_to(:new).with(1).arguments }
+    it { expect(@dispatcher).to respond_to(:dispatch).with(1).arguments }
+  end
+
+  describe ".dispatch" do
+    it "provides no implementation of the method" do
+      payload = ""
+      expect { @dispatcher.dispatch(payload) }.to raise_exception("Not implemented yet.")
+    end
+  end
+end
