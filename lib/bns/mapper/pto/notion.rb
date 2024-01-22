8c74aafd00aa6d50048102cb15fb364f0f7344be
Lorenzo Zuluaga <lorenzo.zuluaga@kommit.co>
2024-01-21 20:21:28 -0500

    Add implementation for PTO's use case

---
+++ lib/bns/mapper/pto/notion.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative "../../domain/pto"
+require_relative "../base"
+
+module Mapper
+  module Pto
+    class Notion
+      include Base
+
+      def map(data)
+        data.map do |pto|
+          Domain::Pto.new(pto["name"], format_date(pto["from"]), format_date(pto["to"]))
+        end
+      end
+
+      private
+
+      def format_date(str_date)
+        return "" if str_date.nil?
+
+        if str_date.include?("T")
+          format = "%Y-%m-%d|%I:%M %p"
+          datetime = Time.new(str_date)
+          datetime.strftime(format)
+        else
+          str_date
+        end
+      end
+    end
+  end
+end
--- lib/bns/use_cases/use_cases.rb
+++ lib/bns/use_cases/use_cases.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "../fetcher/birthday/notion"
-require_relative "../dispatcher/discord"
 require_relative "../mapper/birthday/notion"
 require_relative "../formatter/birthday/discord"
+
+require_relative "../fetcher/pto/notion"
+require_relative "../mapper/pto/notion"
+require_relative "../formatter/pto/discord"
+
+require_relative "../dispatcher/discord"
 require_relative "use_case"
 
 module UseCases
@@ -12,6 +17,17 @@ module UseCases
       fetcher: Fetcher::Birthday::Notion.new(options[:fetch_options]), # !TODO: Use a class for specific configs for fetcher and dispatcher, after everything is working
       mapper: Mapper::Birthday::Notion.new,
       formatter: Formatter::Birthday::Discord.new,
+      dispatcher: Dispatcher::Discord.new(options[:dispatch_options])
+    }
+
+    UseCases::UseCase.new(options)
+  end
+
+  def self.notify_pto_from_notion_to_discord(options)
+    options = {
+      fetcher: Fetcher::Pto::Notion.new(options[:fetch_options]),
+      mapper: Mapper::Pto::Notion.new,
+      formatter: Formatter::Pto::Discord.new,
       dispatcher: Dispatcher::Discord.new(options[:dispatch_options])
     }
 
