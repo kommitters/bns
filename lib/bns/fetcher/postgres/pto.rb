5c13f78e744338035df02ef0a8034b0d74f537cb
FelipeGuzmanSierra <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2024-02-13 16:10:38 -0500

    Update documentation

--- lib/bns/fetcher/postgres/pto.rb
+++ lib/bns/fetcher/postgres/pto.rb
@@ -15,6 +15,11 @@ module Fetcher
     class Pto < Base
       attr_reader :connection, :query
 
+      # Initializes the Postgres fetcher with essential configuration parameters.
+      #
+      # <b>connection config</b> : valid format are the same as the ones defined on the pg gem (https://deveiate.org/code/pg/PG/Connection.html)
+      # <br>
+      # <b>query format</b> : valid format are the same as the ones defined on the pg gem (https://deveiate.org/code/pg/PG/Connection.html)
       def initialize(config)
         super(config)
 
@@ -22,6 +27,11 @@ module Fetcher
         @query = config[:query]
       end
 
+      # Implements the data fetching logic for PTO's data from a Postgres database. It use the PG gem
+      # to request data from a local or external database and returns a validated response.
+      #
+      # Gem: pg (https://rubygems.org/gems/pg/versions/1.5.4)
+      #
       def fetch
         pg_connection = PG::Connection.new(connection)
 
