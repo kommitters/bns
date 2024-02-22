ad39025d438641abb3f15691263ef137817a3653
FelipeGuzmanSierra <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2024-02-22 11:32:54 -0500

    Refactor notion mapper

--- lib/bns/mapper/notion/birthday.rb
+++ lib/bns/mapper/notion/birthday.rb
@@ -11,6 +11,8 @@ module Mapper
     class Birthday
       include Base
 
+      BIRTHDAY_PARAMS = ["Complete Name", "BD_this_year"].freeze
+
       # Implements the logic for shaping the results from a fetcher response.
       #
       # <br>
@@ -27,7 +29,7 @@ module Mapper
         normalized_notion_data = normalize_response(notion_response.results)
 
         normalized_notion_data.map do |birthday|
-          Domain::Birthday.new(birthday["name"], birthday["birth_date"])
+          Domain::Birthday.new(birthday["Complete Name"], birthday["BD_this_year"])
         end
       end
 
@@ -36,32 +38,22 @@ module Mapper
       def normalize_response(results)
         return [] if results.nil?
 
-        normalized_results = []
-
         results.map do |value|
-          properties = value["properties"]
-          properties.delete("Name")
+          birthday_fields = value["properties"].slice(*BIRTHDAY_PARAMS)
 
-          normalized_value = normalize(properties)
+          birthday_fields.each do |field, birthday_value|
+            birthday_fields[field] = extract_birthday_value(field, birthday_value)
+          end
 
-          normalized_results.append(normalized_value)
+          birthday_fields
         end
-
-        normalized_results
       end
 
-      def normalize(properties)
-        normalized_value = {}
-
-        properties.each do |k, v|
-          if k == "Complete Name"
-            normalized_value["name"] = extract_rich_text_field_value(v)
-          elsif k == "BD_this_year"
-            normalized_value["birth_date"] = extract_date_field_value(v)
-          end
+      def extract_birthday_value(field, value)
+        case field
+        when "Complete Name" then extract_rich_text_field_value(value)
+        when "BD_this_year" then extract_date_field_value(value)
         end
-
-        normalized_value
       end
 
       def extract_rich_text_field_value(data)
