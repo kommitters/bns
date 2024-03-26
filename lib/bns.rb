821931f5c6e5028faf1179cd4ec95a9d74b03606
Felipe Guzmán Sierra <97761783+FelipeGuzmanSierra@users.noreply.github.com>
2024-03-26 14:19:40 -0500

    Add deprecatios warning and update readme (#52)

--- lib/bns.rb
+++ lib/bns.rb
@@ -6,4 +6,7 @@ require_relative "bns/use_cases/use_cases"
 module Bns # rubocop:disable Style/Documentation
   include UseCases
   class Error < StandardError; end
+
+  warn "[DEPRECATION] This gem has been renamed to bas and will no\
+  longer be supported. Please switch to bas as soon as possible."
 end
